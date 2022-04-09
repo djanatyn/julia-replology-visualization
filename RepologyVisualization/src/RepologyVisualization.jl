e371eeb485a2f3de37ce209fb3b828a472ea7ad7
Jonathan Strickland <djanatyn@gmail.com>
2022-04-09 13:06:10 -0400

    start populating RepologyVisualization.jl

--- RepologyVisualization/src/RepologyVisualization.jl
+++ RepologyVisualization/src/RepologyVisualization.jl
@@ -1,7 +1,13 @@
 module RepologyVisualization
 
 using JSON
+using Logging
 
-packages = JSON.parse(open("packages.json"))
+packages()::Dict{String, Any} = JSON.parse(open("packages.json"))
+
+base_api_url = "https://repology.org/api/v1"
+
+api_url()::String = "$(base_api_url)/projects/?repos=5-&newest=True"
+api_url(start::String)::String = "$(base_api_url)/projects/$(start)?repos=5-&newest=True"
 
 end # module
