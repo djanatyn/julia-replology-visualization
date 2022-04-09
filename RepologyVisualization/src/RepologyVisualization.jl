3ddd51eaa20d0ca228dd410273112cd86509f1f5
Jonathan Strickland <djanatyn@gmail.com>
2022-04-09 14:59:39 -0400

    run api_url during string interpolation
    now i'm really scripting!

--- RepologyVisualization/src/RepologyVisualization.jl
+++ RepologyVisualization/src/RepologyVisualization.jl
@@ -11,9 +11,12 @@ base_api_url = "https://repology.org/api/v1"
 api_url()::String = "$(base_api_url)/projects/?repos=5-&newest=True"
 api_url(start::String)::String = "$(base_api_url)/projects/$(start)?repos=5-&newest=True"
 
+download_page()::Dict{String, Any} = JSON.parse(read(`curl $(api_url())`, String))
+download_page(start::String)::Dict{String, Any} = JSON.parse(read(`curl $(api_url(start))`, String))
+
 @testset "generate urls" begin
     @test api_url() == "https://repology.org/api/v1/projects/?repos=5-&newest=True"
     @test api_url("package") == "https://repology.org/api/v1/projects/package?repos=5-&newest=True"
 end
 
-end # module
+end
