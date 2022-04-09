0d33a1252f3e4633abac2d3e70070c13155ec86a
Jonathan Strickland <djanatyn@gmail.com>
2022-04-09 13:29:53 -0400

    add tests
    ❯ julia -L RepologyVisualization/src/RepologyVisualization.jl
    Test Summary: | Pass  Total
    generate urls |    2      2

--- RepologyVisualization/src/RepologyVisualization.jl
+++ RepologyVisualization/src/RepologyVisualization.jl
@@ -2,6 +2,7 @@ module RepologyVisualization
 
 using JSON
 using Logging
+using Test
 
 packages()::Dict{String, Any} = JSON.parse(open("packages.json"))
 
@@ -9,5 +10,10 @@ base_api_url = "https://repology.org/api/v1"
 
 api_url()::String = "$(base_api_url)/projects/?repos=5-&newest=True"
 api_url(start::String)::String = "$(base_api_url)/projects/$(start)?repos=5-&newest=True"
+
+@testset "generate urls" begin
+    @test api_url() == "https://repology.org/api/v1/projects/?repos=5-&newest=True"
+    @test api_url("package") == "https://repology.org/api/v1/projects/package?repos=5-&newest=True"
+end
 
 end # module
