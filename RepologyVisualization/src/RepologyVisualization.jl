e1d62232c298f7be1655717c39c2800aa0242b6e
Jonathan Strickland <djanatyn@gmail.com>
2022-04-10 16:32:19 -0400

    support fetching licenses

--- RepologyVisualization/src/RepologyVisualization.jl
+++ RepologyVisualization/src/RepologyVisualization.jl
@@ -3,27 +3,30 @@ module RepologyVisualization
 using JSON
 using Test
 
-struct PackageList
-    page::Dict{String, Any}
-end
-
-packages()::PackageList = PackageList(JSON.parse(open("packages.json")))
+packages() = PackageList(JSON.parse(open("packages.json")))
 
 base_api_url = "https://repology.org/api/v1"
 
 api_url()::String =
     "$(base_api_url)/projects/?repos=5-&newest=True"
-
 api_url(start::String)::String =
     "$(base_api_url)/projects/$(start)?repos=5-&newest=True"
 
-download_page()::PackageList =
-    PackageList(JSON.parse(read(`curl $(api_url())`, String)))
-download_page(start::String)::Dict{String, Any} =
-    PackageList(JSON.parse(read(`curl $(api_url(start))`, String)))
+download_page() =
+    JSON.parse(read(`curl $(api_url())`, String))
+download_page(start::String) =
+    JSON.parse(read(`curl $(api_url(start))`, String))
 
-next_page(packages::PackageList)::String =
-   sort(collect(keys(packages.page)))[end]
+next_page(packages) =
+   sort(collect(keys(packages)))[end]
+
+last_page(start, packages) =
+    next_page(packages) == start
+
+find_licenses(page) = unique(reduce(vcat, [
+    [Dict(:package => package, :license => definition["licenses"])
+     for definition in repos if haskey(definition, "licenses")]
+    for (package, repos) in page]))
 
 @testset "generate urls" begin
     @test api_url() == "https://repology.org/api/v1/projects/?repos=5-&newest=True"
@@ -31,3 +34,4 @@ next_page(packages::PackageList)::String =
 end
 
 end # module
+
